3f4f346d9fd815c498a733f504e617dfb33803f9
Supriya Thakur <supriyathakur@Supriyas-MacBook-Pro.local>
2026-02-02 12:06:34 +0530

    Use Streamlit secrets for Firebase

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,18 @@
+import streamlit as st
 import pyrebase
 
 firebase_config = {
-    "apiKey": "AIzaSyDgpCUGHw3jjzOCzCxw2CZlCbMUgk855ic",
-    "authDomain": "faceapp-874e7.firebaseapp.com",
-    "projectId": "faceapp-874e7",
-    "storageBucket": "faceapp-874e7.firebasestorage.app",
-    "messagingSenderId": "792623592191",
-    "appId": "1:792623592191:web:9c0f2c0f7b60e49ad512ca",
-    "databaseURL": "https://faceapp-874e7-default-rtdb.firebaseio.com/"
+    "apiKey": st.secrets["apiKey"],
+    "authDomain": st.secrets["authDomain"],
+    "databaseURL": st.secrets["databaseURL"],
+    "projectId": st.secrets["projectId"],
+    "storageBucket": st.secrets["storageBucket"],
+    "messagingSenderId": st.secrets["messagingSenderId"],
+    "appId": st.secrets["appId"],
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
-
 auth = firebase.auth()
 db = firebase.database()
+
 
